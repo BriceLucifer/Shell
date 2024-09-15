00de3d2366af96708dea0a0116672098895afec7
BriceLucifer <2376671337@qq.com>
2024-09-16 01:23:45 +0800

    update hello interface

--- shell/src/argsenv.rs
+++ shell/src/argsenv.rs
@@ -1,7 +1,7 @@
 // 处理环境参数
 
 // 环境变量函数
-pub fn args_fun()->Vec<String>{
-    let args:Vec<String> = std::env::args().collect();
+pub fn args_fun() -> Vec<String> {
+    let args: Vec<String> = std::env::args().collect();
     args
 }
--- shell/src/main.rs
+++ shell/src/main.rs
@@ -2,35 +2,64 @@ mod buildin;
 use buildin::*;
 mod argsenv;
 use argsenv::args_fun;
+use cfonts::{say, Colors, Options};
 use colored::*;
-use std::io::{self,Write};
+use std::io::{self, Write};
 
-fn main(){
+fn interface() {
+    say(Options {
+        text: String::from("HI Rust"),
+        font: cfonts::Fonts::FontBlock,
+        colors: vec![Colors::BlueBright, Colors::WhiteBright],
+        align: cfonts::Align::Center,
+        line_height: 1,
+        max_length: 15,
+        ..Options::default()
+    });
+    say(Options {
+        text: String::from("Start your shell journey. By Brice"),
+        font: cfonts::Fonts::FontChrome,
+        align: cfonts::Align::Left,
+        colors: vec![cfonts::Colors::Magenta,cfonts::Colors::MagentaBright],
+        line_height: 1,
+        max_length: 100,
+        version: true,
+        ..Options::default()
+    })
+}
 
+fn main() {
     // 初始界面
-    println!("{}","a shell written in rust🦀 By Brice✨".bright_cyan());
-    println!("------------------------------------------");
-    println!("start your shell journey😊");
-    println!("\n");
+    interface();
 
     // 环境参数
     let _args = args_fun();
 
-    let build_in_strs:Vec<String> = vec!["cd".to_string(),"help".to_string(),"ls".to_string(),"cat".to_string(),"wc".to_string(),"exit".to_string()];
-    let buildin_funcs:Vec<BuildinFntype> = vec![rsh_cd,rsh_help,rsh_ls,rsh_cat,rsh_wc,rsh_exit];
+    let build_in_strs: Vec<String> = vec![
+        "cd".to_string(),
+        "help".to_string(),
+        "ls".to_string(),
+        "cat".to_string(),
+        "wc".to_string(),
+        "exit".to_string(),
+    ];
+    let buildin_funcs: Vec<BuildinFntype> =
+        vec![rsh_cd, rsh_help, rsh_ls, rsh_cat, rsh_wc, rsh_exit];
     // 内置结构体
-    let build_ins = BuildIn{
-        build_in_str:build_in_strs.clone(),
-        build_in_func:buildin_funcs.clone(),
+    let build_ins = BuildIn {
+        build_in_str: build_in_strs.clone(),
+        build_in_func: buildin_funcs.clone(),
     };
 
     // 循环一直运行 类似之前c语言单独我写的bsh_loop()
-    loop{
-        print!("{}","💕🦀rsh -> ".red().yellow());
+    loop {
+        print!("{}", "💕🦀rsh -> ".red().yellow());
         io::stdout().flush().unwrap();
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Failed to read line");
-        if input.trim().is_empty(){
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        if input.trim().is_empty() {
             continue;
         }
         let args: Vec<String> = input.split_whitespace().map(String::from).collect();
@@ -38,5 +67,4 @@ fn main(){
             rsh_execute(&args, &build_ins);
         }
     }
-    
 }
